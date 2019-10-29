9d1e33ddae2b8b5b043bc0cc325d5e3d9d83c535
liuxinye <1170806961@qq.com>
2019-10-29 10:10:56 +0800

    ainimation

---
+++ lib/prictice/animation/animated_decorated_box1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AnimatedDecoratedBox1 extends StatefulWidget {
+  AnimatedDecoratedBox1({Key key}) : super(key: key);
+
+  @override
+  _AnimatedDecoratedBox1State createState() => _AnimatedDecoratedBox1State();
+}
+
+class _AnimatedDecoratedBox1State extends State<AnimatedDecoratedBox1> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/prictice/animation/transition_widget_route.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TranstionWidgetRoute extends StatefulWidget {
+  TranstionWidgetRoute({Key key}) : super(key: key);
+
+  @override
+  _TranstionWidgetRouteState createState() => _TranstionWidgetRouteState();
+}
+
+class _TranstionWidgetRouteState extends State<TranstionWidgetRoute> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
