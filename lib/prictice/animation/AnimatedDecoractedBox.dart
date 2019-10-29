d75e53b9b796925e0f6ff8b9b6c9e748490ae9d3
liuxinye <1170806961@qq.com>
2019-10-29 21:36:59 +0800

    add

---
+++ lib/prictice/animation/AnimatedDecoractedBox.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AnimatedDecoratedBox extends StatefulWidget {
+  AnimatedDecoratedBox({Key key}) : super(key: key);
+
+  @override
+  _AnimatedDecoratedBoxState createState() {
+    return _AnimatedDecoratedBoxState();
+  }
+}
+
+class _AnimatedDecoratedBoxState extends State<AnimatedDecoratedBox> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        appBar: AppBar(
+
+        ),
+    );
+  }
+}
+
