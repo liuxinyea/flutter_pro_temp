da636e83fa258aae8ce25e81ffbe48b249039570
liuxinye <1170806961@qq.com>
2019-11-12 15:20:01 +0800

    ios环境配置

--- lib/host.dart
+++ lib/host.dart
@@ -125,7 +125,7 @@ class _HostPage extends State<HostPage>{
         bottomNavigationBar:new BottomNavigationBar(
           items: <BottomNavigationBarItem>[
             BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('官方Demo')),
-            BottomNavigationBarItem(icon: Icon(Icons.business), title: Text('练习')),
+            BottomNavigationBarItem(icon: Icon(Icons.business), title: Text('UI练习')),
             BottomNavigationBarItem(icon: Icon(Icons.camera), title: Text('功能')),
             BottomNavigationBarItem(icon: Icon(Icons.account_circle), title: Text('我的')),
           ],
